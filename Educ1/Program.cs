f5f1fc1d5f69a0c8f53ecadf8bf622ca74f2c395
Infiell <30868449+Infiell@users.noreply.github.com>
2024-11-20 13:08:57 +0400

    Upload

---
+++ Educ1/Program.cs
@@ -0,0 +1,12 @@
+﻿int repetitions;
+string message;
+
+Console.Write("Введите сообщение для вывода: ");
+message = Console.ReadLine();
+Console.Write("Введите количество выводов данного сообщения: ");
+repetitions = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < repetitions; i++)
+{
+    Console.WriteLine(message);
+}
---
+++ Educ2/Program.cs
@@ -0,0 +1,10 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите exit для выхода из цикла.");
+    string message = Console.ReadLine();
+
+    if (message == "exit")
+    {
+        break;
+    }
+}
